35ed405c4a89799afc8d5d4e6d983974b6a288a5
Henry Bentil <hbentil@acsint.io>
2024-10-25 12:18:03 +0000

    first commit

---
+++ src/redux/app/appSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const initialState = {
+    loading: false,
+    error: false,
+    config:{}
+};
+
+const appSlice = createSlice({
+    name: 'app',
+    initialState,
+    reducers: {
+        setAppLoading: (state, action) => {
+            state.loading = action.payload;
+        },
+        setFrontendConfig: (state, action) => {
+            state.config = action.payload;
+        }
+    },
+});
+
+export const { setAppLoading, setFrontendConfig } = appSlice.actions;
+export default appSlice.reducer;
