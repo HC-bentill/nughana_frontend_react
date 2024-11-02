e341ed8b362436b2246b2e848cd47981515cfcce
Henry Bentil <henrybentil88@gmail.com>
2024-11-02 07:57:18 +0000

    started home page polishing

--- src/pages/homepage/stay_connected.jsx
+++ src/pages/homepage/stay_connected.jsx
@@ -9,16 +9,16 @@ function StayConnected() {
     <>
       <section>
         <div className="flex justify-center">
-          <div className="bg-[#101010] w-full text-white text-center p-20">
+          <div className="bg-[#101010] w-full text-white text-center md:py-20 md:px-20 px-4 py-20">
             <div className="flex justify-center">
-              <h3 className="text-5xl">Stay connected with the African</h3>
+              <h3 className="text-[29px] md:text-5xl">Stay connected with the African <span className="md:hidden" >Culture</span></h3>
             </div>
-            <div className="flex justify-center mr-60">
-              <Underline className="w-56" />
+            <div className="flex justify-center md:mr-60 md:-mt-0 -mt-[47px]">
+              <Underline className="md:w-56 w-[200px]" />
             </div>
 
-            <h3 className="text-5xl mb-7">culture</h3>
-            <p className="">
+            <h3 className="text-[29px] md:block hidden md:text-5xl mb-7">culture</h3>
+            <p className="md:text-[14px] text-[13px] md:mt-0 mt-[80px]">
             Here at NuGhana, we connect you to everything Ghana regardless of your location.
             </p>
           </div>
